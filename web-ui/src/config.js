5d9dda73f432ba2c11e040da9f72fb8d8222a1d5
theankeong <theankeong@gmail.com>
2021-03-12 20:11:51 +0800

    init

---
+++ web-ui/src/config.js
@@ -0,0 +1,9 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+// Amazon IVS Playback URL
+// Replace this with your own Amazon IVS Playback URL
+export const PLAYBACK_URL = "https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.DmumNckWFTqz.m3u8";
+
+// Chat websocket address
+export const CHAT_WEBSOCKET = "wss://b0r30uywca.execute-api.us-east-1.amazonaws.com/Prod";
